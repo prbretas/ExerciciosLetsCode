629085ecc8b54207050d83aca138cf56b9611337
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-12-13 14:02:55 -0300

    do while try catch

--- ExerciciosLetsCode/ExerciciosLetsCode/src/Exercicios121222/Tabuada.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/Exercicios121222/Tabuada.java
@@ -10,17 +10,35 @@ public class Tabuada {
         System.out.println("Olá seja bem vindo!");
         System.out.println("Este sistema irá calcular a Tabuada");
 
-        System.out.println("Digite abaixo um número");
-        int numero = Input.nextInt();
-        System.out.println("Tabuada do número: " + numero);
+        int numero = 0;
+        boolean continuar;
 
-        for(int i = 0; i < 11; i++){
+
+        do {
+            continuar = false;
+            try {
+                Input = new Scanner(System.in);
+                System.out.println();
+                System.out.println("Digite um número abaixo :");
+                numero = Input.nextInt();
+                System.out.println("Tabuada do número: " + numero);
+                continuar = true;
+
+
+
+            } catch (Exception e) {
+                System.out.println("ERRO: Digite um valor válido");
+
+            }
+
+        } while (!continuar);
+
+
+        for (int i = 0; i < 11; i++) {
             System.out.print(numero + " x " + i + " = ");
             System.out.println(i * numero);
 
-
         }
-
 
     }
 }
