4866ccba9fd7ef9d0b6abec302fa410441cee7f3
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-12-14 16:24:15 -0300

    fazendo os tratamentos try catch e do while

--- ExerciciosLetsCode/ExerciciosLetsCode/src/AULA141222/Calculadora.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/AULA141222/Calculadora.java
@@ -4,55 +4,110 @@ import java.util.Scanner;
 
 public class Calculadora {
     Scanner input = new Scanner(System.in);
-//----------------------- MÉTODOS - ATRIBUTOS-------------------------------------------------
+
     double numero1;
-    String op;
+    int op;
     double numero2;
     double resultado;
-//----------------------- MÉTODOS - FUNÇÕES-------------------------------------------------
+
+
     public void entradaDados() {
-        System.out.println("Digite um NUMERO:");
-        numero1 = input.nextDouble();
 
-        System.out.println("Digite a Operação:");
-        op = input.next();
+        boolean continuar;
+        do {
+            continuar = false;
+            try {
+                input = new Scanner(System.in);
+                System.out.println("Digite um NUMERO:");
+                numero1 = input.nextDouble();
+                continuar = true;
 
-        System.out.println("Digite outro NUMERO:");
-        numero2 = input.nextDouble();
+            } catch (Exception e) {
+                System.out.println("ERRO: Digite um NÚMERO!");
+            }
 
-        switch (op) {
-            case "+":
-                somarNumeros();
-                System.out.println("O resultado é: " + resultado);
-                break;
-            case "-":
-                subtrairNumeros();
-                System.out.println("O resultado é: " + resultado);
-                break;
-            case "*":
-                multiplicarNumeros();
-                System.out.println("O resultado é: " + resultado);
-                break;
-            case "/":
-                dividirNumeros();
-                System.out.println("O resultado é: " + resultado);
-                break;
-        }
+        } while (!continuar);
+
+        do {
+            continuar = false;
+            try {
+                input = new Scanner(System.in);
+                System.out.println("Digite a Operação: ");
+                System.out.println("1 - SOMAR (+)");
+                System.out.println("2 - SUBTRAIR (-)");
+                System.out.println("3 - MULTIPLICAR (*)");
+                System.out.println("4 - DIVIDIR (/)");
+                op = input.nextInt();
+
+                switch (op) {
+                    case 1:
+                    somarNumeros();
+                    System.out.println("---------------------------");
+                    System.out.println("O resultado é: " + resultado);
+                    System.out.println("---------------------------");
+                    break;
+                    case 2:
+                    subtrairNumeros();
+                    System.out.println("---------------------------");
+                    System.out.println("O resultado é: " + resultado);
+                    System.out.println("---------------------------");
+                    break;
+                    case 3:
+                    multiplicarNumeros();
+                    System.out.println("---------------------------");
+                    System.out.println("O resultado é: " + resultado);
+                    System.out.println("---------------------------");
+                    break;
+                    case 4:
+                    dividirNumeros();
+                    System.out.println("---------------------------");
+                    System.out.println("O resultado é: " + resultado);
+                    System.out.println("---------------------------");
+                    break;
+                    default:
+                        System.out.println("DIGITE UMA OPERAÇÃO VÁLIDA");
+                        continuar = true;
+                }
+            } catch (Exception e) {
+                System.out.println("ERRO: Digite uma OPERAÇÃO válida!");
+            }
+        } while (!continuar);
+
+
+        do {
+            continuar = false;
+            try {
+                input = new Scanner(System.in);
+                System.out.println("Digite outro NUMERO:");
+                numero2 = input.nextDouble();
+                continuar = true;
+            } catch (Exception e) {
+                System.out.println("ERRO: Digite um NÚMERO!");
+            }
+        } while (!continuar);
+
+
     }
-    public double somarNumeros(){
-       resultado = numero1 + numero2;
-       return  resultado;
+
+    public double somarNumeros() {
+        resultado = numero1 + numero2;
+        return resultado;
     }
-    public double subtrairNumeros(){
+
+    public double subtrairNumeros() {
         resultado = numero1 - numero2;
-        return  resultado;
+        return resultado;
     }
-    public double multiplicarNumeros(){
+
+    public double multiplicarNumeros() {
         resultado = numero1 * numero2;
-        return  resultado;
+        return resultado;
     }
-    public double dividirNumeros(){
+
+    public double dividirNumeros() {
         resultado = numero1 / numero2;
-        return  resultado;
+        return resultado;
     }
+
+
 }
