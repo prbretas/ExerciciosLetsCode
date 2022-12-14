157c3bcb6d2134472678cac1958289d50abc7085
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-12-14 15:55:31 -0300

    Update Calculadora.java

--- ExerciciosLetsCode/ExerciciosLetsCode/src/AULA141222/Calculadora.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/AULA141222/Calculadora.java
@@ -4,60 +4,55 @@ import java.util.Scanner;
 
 public class Calculadora {
     Scanner input = new Scanner(System.in);
-
-    double numerador;
+//----------------------- MÉTODOS - ATRIBUTOS-------------------------------------------------
+    double numero1;
     String op;
-    double denominador;
+    double numero2;
     double resultado;
-
-
-
-
-
+//----------------------- MÉTODOS - FUNÇÕES-------------------------------------------------
     public void entradaDados() {
-        System.out.println("Digite o NUMERADOR:");
-        numerador = input.nextDouble();
+        System.out.println("Digite um NUMERO:");
+        numero1 = input.nextDouble();
 
         System.out.println("Digite a Operação:");
         op = input.next();
 
-        System.out.println("Digite o DENOMINADOR");
-        denominador = input.nextDouble();
+        System.out.println("Digite outro NUMERO:");
+        numero2 = input.nextDouble();
 
-
-        if(op.equals("+")){
-           somarNumeros();
-            System.out.println("O resultado é: " + resultado);
-        } else if(op.equals("-")){
-            subtrairNumeros();
-            System.out.println("O resultado é: " + resultado);
-        } else if(op.equals("*")){
-            multiplicarNumeros();
-            System.out.println("O resultado é: " + resultado);
-        } else if(op.equals("/")){
-            dividirNumeros();
-            System.out.println("O resultado é: " + resultado);
+        switch (op) {
+            case "+":
+                somarNumeros();
+                System.out.println("O resultado é: " + resultado);
+                break;
+            case "-":
+                subtrairNumeros();
+                System.out.println("O resultado é: " + resultado);
+                break;
+            case "*":
+                multiplicarNumeros();
+                System.out.println("O resultado é: " + resultado);
+                break;
+            case "/":
+                dividirNumeros();
+                System.out.println("O resultado é: " + resultado);
+                break;
         }
     }
-
     public double somarNumeros(){
-       resultado = numerador + denominador;
+       resultado = numero1 + numero2;
        return  resultado;
     }
     public double subtrairNumeros(){
-        resultado = numerador - denominador;
+        resultado = numero1 - numero2;
         return  resultado;
     }
     public double multiplicarNumeros(){
-        resultado = numerador * denominador;
+        resultado = numero1 * numero2;
         return  resultado;
     }
-
     public double dividirNumeros(){
-        resultado = numerador / denominador;
+        resultado = numero1 / numero2;
         return  resultado;
     }
-
-
-
 }
