6b3414a0b4f7bcf9e37bb5e94ae9725a993db80c
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-12-19 15:46:12 -0300

    atualizando codigo PRIVATE, GET e SET

--- ExerciciosLetsCode/ExerciciosLetsCode/src/Boletim/Boletim2.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/Boletim/Boletim2.java
@@ -3,34 +3,82 @@ package Boletim;
 public class Boletim2 {
 
     //  --------------- ATRIBUTOS ---------------
-    String nome;
-    String disciplina;
-    double nota1;
-    double nota2;
-    double nota3;
-    double nota4;
-    double media;
-
-
-
+    private String _nome;
+    private String _disciplina;
+    private double _nota1;
+    private double _nota2;
+    private double _nota3;
+    private double _nota4;
+    private double _media;
     //  -------------- PROPRIEDADES --------------------
+
+    // -------------------------------------------------
+    // CONSTRUÇÃO DOS METODOS ACESSORES - ACESSAR ATRIBUTOS
+    // SET - METODO QUE PERMITE ALTERAR O VALOR DO ATRIBUTO (ALTERAR)
+    // SEMPRE SERA VOID
+
+    public void setNome(String nome) {
+        this._nome = nome;
+    }
+    public void setDisciplina(String disciplina) {
+        this._disciplina = disciplina;
+    }
+    public void setNota1(double nota1) {
+        this._nota1 = nota1;
+    }
+    public void setNota2(double nota2) {
+        this._nota2 = nota2;
+    }
+    public void setNota3(double nota3) {
+        this._nota3 = nota3;
+    }
+    public void setNota4(double nota4) {
+        this._nota4 = nota4;
+    }
+    public void setMedia(double media) {
+        this._media = media;
+    }
+
+
+
+    //GET - METODO QUE PERMITE ACESSAR, VISUALIZAR O VALOR DO ATRIBUTO
+    // SEMPRE SERA DO TIPO DO ATRIBUTO
+
+    public String getNome(){
+        return this._nome;
+    }
+    public String getDisciplina(){
+        return this._disciplina;
+    }
+    public double getNota1(){
+        return this._nota1;
+    }
+    public double getNota2(){
+        return this._nota2;
+    }
+    public double getNota3(){
+        return this._nota3;
+    }
+    public double getNota4(){
+        return this._nota4;
+    }
+    public double getMedia(){
+        return this._media;
+    }
 
 
     //  -------------- MÉTODOS / FUNÇÕES --------------------
     // VOID - Não tem RETORNO - TEM Q SER RESOLVIDO DENTRO DO METODO
-    public void calcularMediaVoid(){
-         media = (nota1 + nota2 + nota3 + nota4) / 4;
-        System.out.println("VOID: A média do aluno é: " + media);
+    public void calcularMediaVoid() {
+        _media = (_nota1 + _nota2 + _nota3 + _nota4) / 4;
+        System.out.println("VOID: A média do aluno é: " + _media);
     }
 
     // ESSE METODO TEM RETORNO DO TIPO DOUBLE
-    public double calcularMediaReturn(){
-        media = (nota1 + nota2 + nota3 + nota4) / 4;
-        return media;
+    public double calcularMediaReturn() {
+        _media = (_nota1 + _nota2 + _nota3 + _nota4) / 4;
+        return _media;
     }
-
-
-
 
 
 }
--- ExerciciosLetsCode/ExerciciosLetsCode/src/Boletim/MainBoletim.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/Boletim/MainBoletim.java
@@ -12,12 +12,29 @@ public class MainBoletim {
         Scanner input = new Scanner(System.in);
         Boletim2 aluno1 = new Boletim2();
 
-        aluno1.nome = "João";
+        // ANTES DE CRIAR O GET E SET
+     /* aluno1.nome = "João";
         aluno1.disciplina = "Português";
         aluno1.nota1 = 7.8;
         aluno1.nota2 = 8.9;
         aluno1.nota3 = 9.4;
         aluno1.nota4 = 5.7;
+      */
+
+        //ENTRADA DE DADOS VIA SET
+
+        aluno1.setNome("João");
+        aluno1.setDisciplina("Matemática");
+        aluno1.setNota1(10);
+        aluno1.setNota2(7.43);
+        aluno1.setNota3(6.75);
+        aluno1.setNota4(5.88);
+
+        // ENTRADA DE DADOS VIA SET SCANNER
+        System.out.println("Digite o nome do aluno:");
+        aluno1.setNome(input.nextLine());
+        // SAÍDA DE DADOS VIA GET, usando SOUT
+        System.out.println(aluno1.getMedia());
 
         // VOID - ja esta resolvido no metodo, nao precisa colocar system out
         aluno1.calcularMediaVoid();
@@ -26,7 +43,5 @@ public class MainBoletim {
         System.out.println("FUNÇÃO TIPADA: A média do aluno é: " + aluno1.calcularMediaReturn());
 
 
-
-        
     }
 }
