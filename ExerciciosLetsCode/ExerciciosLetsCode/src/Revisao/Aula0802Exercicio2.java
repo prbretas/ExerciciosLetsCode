fac20d41e853263e9d4d7cab8fdfc290f781cda0
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2023-02-08 10:22:12 -0300

    update exercicio 2

--- ExerciciosLetsCode/ExerciciosLetsCode/src/Revisao/Aula0802Exercicio2.java
+++ ExerciciosLetsCode/ExerciciosLetsCode/src/Revisao/Aula0802Exercicio2.java
@@ -1,20 +1,5 @@
 package Revisao;/*
 
-Exercício 1
-Criar um programa que recebe uma lista de palavras do usuário.
-Deve-se pedir uma nova palavra até que o usuário insira uma string vazia.
-
-Ao final, deve-se imprimir apenas a palavra que ficaria
-na última posição se a lista estivesse ordenada alfabeticamente.
-
-Exemplo de execução:
-Digite uma palavra: Zezinho
-Digite uma palavra: Maria
-Digite uma palavra: André
-Digite uma palavra:
-
-Zezinho
-
 -----------------------------------------------------------------------------------
 Exercício 2
 Crie um programa que receba duas palavras e determine se elas são um anagrama.
@@ -29,57 +14,32 @@ xicara
 cachimbo
 
 false
-
-* */
+*/
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Aula0802Exercicio1 {
+public class Aula0802Exercicio2 {
     public static void main(String[] args) {
-        System.out.println("Digite uma palavra:");
-        ArrayList<String> palavras = lerListaPalavras();
-
-
-    //    String palavra1 = "jose";
-  //      String palavra2 = "ze";
-
-//        System.out.println(palavra1.compareTo(palavra2));
-
-
-      String ultimaPalavra= palavras.get(0);
-
-      for(String palavra : palavras){
-          if(ultimaPalavra.toLowerCase().compareTo(palavra.toLowerCase()) < 0){
-              ultimaPalavra = palavra;
-          }
-      }
-        System.out.println();
-        System.out.println("Essa foi a última palavra digita em ordem afabética : " + ultimaPalavra);
-    }
-
-
-
-    private static ArrayList<String> lerListaPalavras(){
         Scanner scanner = new Scanner(System.in);
-        //LISTA ARRAY
-        ArrayList<String> palavras = new ArrayList<>();
-        //String[] palavras = new String[];
-        String input = scanner.nextLine();
-        // !input.isEmpty() ---> !input.equals("")
-        //int i =0;
 
-        while(!input.isEmpty()){
-            palavras.add(input);
-            System.out.println("Digite uma palavra:");
-            input = scanner.nextLine();
+        System.out.println("Digite palavra 1: ");
+        String palavra1 = scanner.nextLine();
+        System.out.println("Digite palavra 2: ");
+        String palavra2 = scanner.nextLine();
+
+        ArrayList<Character> lista1 = new ArrayList<>();
+        ArrayList<Character> lista2 =  new ArrayList<>();
+
+        for(char letra : palavra1.toCharArray()){
+            lista1.add(letra);
         }
 
-        for(String palavra :palavras){
-            System.out.println(palavra);
+        for(char letra : palavra2.toCharArray()){
+            lista1.add(letra);
         }
-        return palavras;
+
+
     }
-
 }
 
